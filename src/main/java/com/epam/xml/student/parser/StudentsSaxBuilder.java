ecef88440bf89ba136289a21a5003f72eba806d1
Dns911 <vasilevskydenis@gmail.com>
2022-10-12 18:31:00 +0300

    12.10

---
+++ src/main/java/com/epam/xml/student/parser/StudentsSaxBuilder.java
@@ -0,0 +1,49 @@
+package com.epam.xml.student.parser;
+
+import com.epam.xml.student.entity.Student;
+import com.epam.xml.student.handler.StudentErrorHandler;
+import com.epam.xml.student.handler.StudentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.util.Set;
+import java.util.StringJoiner;
+
+public class StudentsSaxBuilder {
+
+
+    private Set<Student> students;
+    private StudentHandler handler = new StudentHandler();
+    private XMLReader reader;
+
+
+    public StudentsSaxBuilder() {
+// reader configuration
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        try {
+            SAXParser saxParser = factory.newSAXParser();
+            reader = saxParser.getXMLReader();
+        } catch (ParserConfigurationException | SAXException e) {
+            e.printStackTrace(); // log
+            reader.setErrorHandler(new StudentErrorHandler());
+            reader.setContentHandler(handler);
+        }
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void buildSetStudents(String filename) {
+        try {
+            reader.parse(filename);
+        } catch (IOException | SAXException e) {
+            e.printStackTrace(); // log
+        }
+        students = handler.getStudents();
+    }
+}
---
+++ src/main/java/com/epam/xml/student/validator/BaseValidatorMain.java
@@ -0,0 +1,33 @@
+package com.epam.xml.student.validator;
+import java.io.*;
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import com.epam.xml.student.handler.StudentErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class BaseValidatorMain {
+    public static void main(String[] args) {
+        String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+        String fileName = "data_xml/students.xml";
+        String schemaName = "data_xml/students.xsd";
+        SchemaFactory factory = SchemaFactory.newInstance(language);
+        File schemaLocation = new File(schemaName);
+        try {
+// schema creation
+            Schema schema = factory.newSchema(schemaLocation);
+// creating a schema-based validator
+            Validator validator = schema.newValidator();
+            Source source = new StreamSource(fileName);
+// document check
+            validator.setErrorHandler(new StudentErrorHandler());
+            validator.validate(source);
+        } catch (SAXException | IOException e) {
+            System.err.println(fileName + " is not correct or valid");
+        }
+    }
+}
